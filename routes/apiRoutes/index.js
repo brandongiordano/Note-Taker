aad55d04aaf4b2b9e9082d447e228801b10803cd
brandongiordano <bgiordano9917@aol.com>
2023-01-30 11:43:05 -0500

    completed apiRoutes and notes js

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const {notes} = require("../../db/db.json");
+const {createNewNote, updateDb} = require("../../lib/notes");
+
+router.get("/notes", (req, res) => {
+    let results = notes;
+    res.json(results);
+})
+
+router.post("/notes", (req, res) => {
+    req.bpdy.id = uuidv4();
+    let newNote = createNewNote(req.body, notes);
+    res.json(newNote);
+})
+
+router.delete("/notes/:id", (req, res) => {
+    let params = req.params.id;
+    updateDb(params, notes);
+    res.redirect("");
+})
+
+module.exports = router;
