1c045c6bc1adbed7e71771a63f4e467aeee0c303
brandongiordano <bgiordano9917@aol.com>
2023-01-30 10:39:01 -0500

    created server.js

--- server.js
+++ server.js
@@ -2,3 +2,17 @@ const express = require("express");
 const app = express();
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
